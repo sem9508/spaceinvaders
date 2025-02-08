a91545b7d348d5c0dc0077f96a10be4c188ab429
sem9508 <sem40911@gmail.com>
2025-02-08 20:21:16 +0100

    changed Ship x y ... to rect

--- objects/ship.py
+++ objects/ship.py
@@ -3,10 +3,7 @@ from constants import *
 
 class Ship:
     def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.rect = pygame.Rect(x, y, width, height)
         self.color = color
 
     def draw(self, screen):
