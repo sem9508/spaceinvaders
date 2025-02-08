4d2a76fda0cb4214d26d0f94fb5ee1667af2d160
sem9508 <sem40911@gmail.com>
2025-02-08 20:22:46 +0100

    update to button

--- objects/button.py
+++ objects/button.py
@@ -3,17 +3,13 @@ from constants import *
 
 class Button:
     def __init__(self, x, y, width, height, path):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect = pygame.Rect(x, y, width, height)
         self.image = pygame.image.load(path).convert_alpha()
         self.image = pygame.transform.scale(self.image, (width, height))
 
 
     def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
     def update(self, mouse):
         if self.rect.collidepoint(mouse[0], mouse[1]):
